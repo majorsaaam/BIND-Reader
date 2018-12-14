b2657de4cc3b4a76345a46adb2d3e68f51501801
Samantha Marques <sammy.marques@outlook.com>
2018-12-14 20:02:45 +0000

    Habemus date
    Date parsed successfully from pubDate, image tag and/source is possible to fetch but it will return 401, so that is ticked as well.
    
    More styling will follow, possibly for the categories since we can't safely add images to each card. Lame.

--- rss.js
+++ rss.js
@@ -5,24 +5,38 @@ fetch("https://www.engadget.com/rss-full.xml").then((res) => {
             var dom = new DOMParser();
             let doc = dom.parseFromString(xml, 'text/xml');
             doc.querySelectorAll('item').forEach((item) => {
-                idx++;                
-                if (idx < 11) {
+                idx++;                          /*  idx just to count     */
+                if (idx < 11) {                 /*  and stop the foreach  */
+                    /* creating elements */
                     let card = document.createElement('div');
                     card.className = "card m-3 w-100 p-3 card-fixed-height";
                     let h1 = document.createElement('h1');
-                    h1.className = "h3 font-weight-light";
+                    h1.className = "h3 font-weight-light mb-0";
+                    let byWhen = document.createElement('span');
+                    byWhen.className = "text-muted small mb-3";
                     let bodyTxt = document.createElement('p');
                     bodyTxt.className = "truncated-txt text-muted";
-                    bodyTxt.textContent = item.querySelector('description').textContent.replace(/<\/?[^>]+(>|$)/g, "");
+                    let hrefPost = document.createElement('a');
+                    hrefPost.href = item.querySelector('link').textContent;
+                    hrefPost.innerHTML = "&rarr;";
+                    let date = new Date(item.querySelector('pubDate').textContent);
+                    date = date.toLocaleString('en-US', {month: 'short', day: 'numeric', year: 'numeric'}); 
 
+                    /* appending everything to each card */
+                    h1.appendChild(hrefPost);
                     card.appendChild(h1);
+                    card.appendChild(byWhen);
                     card.appendChild(bodyTxt);
 
+                    /* creating content inside created elements */
                     h1.textContent = item.querySelector('title').textContent;
+                    byWhen.textContent = "By  " + item.querySelector('creator').textContent + " on " + date;
+                    bodyTxt.textContent = item.querySelector('description').textContent.replace(/<\/?[^>]+(>|$)/g, "");
                     document.querySelector("section").appendChild(card);
                 }
             })
         } catch (err) {
+            /* everybody commits mistakes... but you should never pull it to the origin */
             console.log("Error:\n", err);
         };
     })
