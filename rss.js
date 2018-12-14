85188822401883aeda22bdd1462ba1aed7fbfb6c
Samantha Marques <sammy.marques@outlook.com>
2018-12-14 18:53:56 +0000

    Basic cards
    Title is OK and description is neatly truncated. Latest 10 cards showing so that also got ticked. Next step is adding more styling, so far so good.
    
    EDIT: would have added image if it wasn't badly inserted in description. That's also an upcoming feature...

--- rss.js
+++ rss.js
@@ -1,22 +1,29 @@
-
+var idx = 0;
 fetch("https://www.engadget.com/rss-full.xml").then((res) => {    
     res.text().then((xml) => {
-        /* console.log(xml); */
         try {
             var dom = new DOMParser();
             let doc = dom.parseFromString(xml, 'text/xml');
             doc.querySelectorAll('item').forEach((item) => {
-                let card = document.createElement('div');
-                card.className = "card m-3 w-100 p-3";
-                let h1 = document.createElement('h1');
-                h1.className = "h3 font-weight-light";
-                card.appendChild(h1);
-                h1.textContent = item.querySelector('title').textContent;
-                document.querySelector("output").appendChild(card);
-            })
+                idx++;                
+                if (idx < 11) {
+                    let card = document.createElement('div');
+                    card.className = "card m-3 w-100 p-3 card-fixed-height";
+                    let h1 = document.createElement('h1');
+                    h1.className = "h3 font-weight-light";
+                    let bodyTxt = document.createElement('p');
+                    bodyTxt.className = "truncated-txt text-muted";
+                    bodyTxt.textContent = item.querySelector('description').textContent.replace(/<\/?[^>]+(>|$)/g, "");
 
+                    card.appendChild(h1);
+                    card.appendChild(bodyTxt);
+
+                    h1.textContent = item.querySelector('title').textContent;
+                    document.querySelector("section").appendChild(card);
+                }
+            })
         } catch (err) {
-            console.log("Erro ao disponibilizar o feed:\n", err);
-        }
+            console.log("Error:\n", err);
+        };
     })
 })
